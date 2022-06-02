5c5a9a653d4de7b0a23ed7e8241facf575accb29
Voltxr <62946338+Voltxr@users.noreply.github.com>
2022-06-02 04:04:16 +0300

    UserLocation
    AddMethod to get position of user and mark it on map
    Added a button to show  coordinates

--- js/initMap.js
+++ js/initMap.js
@@ -1,4 +1,6 @@
 async function initMap() {
+
+
   //центр відображуваної карти - Холодногірське метро
   var opt = {
     center: { lat: 49.982614, lng: 36.180993 },
@@ -7,6 +9,10 @@ async function initMap() {
 
   const map = new google.maps.Map(document.getElementById("map"), opt); //уся карта
 
+  getUserLocation();
+  var btn = document.getElementById("locationBtn");
+  btn.onclick = getUserLocation; 
+  
   const createMarker = (props) => {
     const markerMetro = new google.maps.Marker({
       map: map,
@@ -41,4 +47,37 @@ async function initMap() {
       icon: icon,
     });
   });
+
+//Знаходження та відмитка юзера на карті(Автоматично робе фокус на домівки користувача)
+function getUserLocation()
+  {
+  navigator.geolocation.getCurrentPosition(
+    function geolocationSuccess(position) {
+      var location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+      var userIcon =
+      {
+        url: "https://icon-library.com/images/small-home-icon/small-home-icon-14.jpg", 
+        scaledSize: new google.maps.Size(40, 40)
+      };
+
+      createMarker({
+        position: {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        },
+        title: "Ви",
+        info: "<h3>Ваша домівка</h3><p>Відчинено: тільки для вас<br></p>",
+        icon: userIcon,
+      });
+
+      map.setCenter(location);
+    },
+    function geolocationFailed(position)
+    {
+      alert("Не вдалося отримати вашу геолокацію")
+    });
+  }
+
 }
+
